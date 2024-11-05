dc489e0738f2b6b3d3e5fd522cb2d434531d9dac
Veeles <kacper.ros2@interia.pl>
2024-11-05 18:46:08 +0100

    Connecting the website to the blockchain API and adding a table of the most values ​​of the most popular cryptocurrencies

--- main.js
+++ main.js
@@ -6,9 +6,24 @@ const port = 3000;
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
+const URL_API = "https://api.blockchain.com/v3/exchange/"
 
-app.get('/', (req, res) => {
-    res.render('index.ejs');
+app.get('/', async (req, res) => {
+    const cryptoSymbols = ["BTC-USD", "ETH-USD", "USDT-USD", "BNB-USD", "USDC-USD", "XRP-USD", "ADA-USD", "SOL-USD", "DOGE-USD", "DOT-USD"]
+    const cryptoValues = []
+    try{ 
+        const result = await axios.get(URL_API + `tickers`);
+        for(let i = 0; i < result.data.length; i++){
+            if (cryptoSymbols.includes(result.data[i].symbol)){
+                cryptoValues.push(result.data[i]);
+            }
+        }
+        console.log(cryptoValues);
+        res.render('index.ejs', {content: cryptoValues});
+
+    } catch (error) {
+        console.log(error);
+    }
 });
 
 app.listen(port, (err) => {
